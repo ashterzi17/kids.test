f153055f275a168f422ebc50dd2561a5feeb033d
Ash Terzi <killthesound17@gmail.com>
2018-09-06 19:55:38 +0300

    slick configured

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+$('.model-slider').slick({
+  arrows: true,
+  dots: true,
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
